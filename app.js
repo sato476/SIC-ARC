18ec6d5c4311049e3af45747662d27112528ce4d
sato476 <56417742+sato476@users.noreply.github.com>
2019-10-15 17:47:28 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const app = express();
 
-var index = require('./routes/index');
-
-var app = express();
-
+const record = require('./routes/record');
+const login = require('./routes/login');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,10 +15,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const session_opt = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie:{ maxAge: 60 * 60 * 1000 }
+};
+
+app.use(session(session_opt));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/', index );
+app.use('/', login );
+app.use('/record', record );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
