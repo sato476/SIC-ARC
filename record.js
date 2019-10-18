9546f7c45594d6e463e31b79d3f2d136ee3bac66
sato476 <56417742+sato476@users.noreply.github.com>
2019-10-18 11:31:55 +0900

    Add files via upload

--- record.js
+++ record.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  if( req.session.login == null ){
+    res.redirect('http://localhost:3000/');
+  }
   res.render('record', {
-    title: '工数・勤怠　登録'
+    title: '工数勤怠　登録'
   });
 });
+
+router.post( '/', function(req, res ,next){
+  console.log(req)
+})
 
 module.exports = router;
