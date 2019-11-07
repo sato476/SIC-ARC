03c2922bed02c664df1b6f6044a8b56cdba983f7
sato476 <56417742+sato476@users.noreply.github.com>
2019-11-07 12:05:12 +0900

    Add files via upload

--- routes/record.js
+++ routes/record.js
@@ -24,6 +24,24 @@ router.post( '/', async function(req, res ,next){
   // if( req.session.login == null ){
   //   res.redirect('http://localhost:3000/');
   // }
+
+  let date_sample = req.body.date.split('-');
+  console.log('year: ' + date_sample[0]);
+  console.log('month: ' + date_sample[1]);
+  console.log('day: ' + date_sample[2]);
+
+  const juniji = new Date('','','','00','00');
+  const goji = new Date('','','','5','00');
+  const asa = new Date('','','','9','00');
+  const teji = new Date('','','','17','45');
+  const juji = new Date('','','','22','00');
+  const juniji2 = new Date('','','1','00','00');
+  const asa2 = new Date('','','1','09','00');
+  const goji2 = new Date('','','1','5','00');
+  const teji2 = new Date('','','1','17','45');
+  const juji2 = new Date('','','1','22','00');
+  
+  
   let i = 0
   let query = [];
   let p =req.body.project_code 
@@ -103,6 +121,13 @@ if(O < I){
   let hikaku_min = 0;
   let pp = req.body.leave_time; 
 
+  let work_time;
+  let long_time;
+  let night_time;
+
+  let work_hour;
+  let work_min;
+
   if(!Array.isArray(pp)){
     pp = 1;
     console.log("配列ではない")
@@ -118,11 +143,121 @@ if(O < I){
   for(let num = 0; num<pp;num++ ){
   LT_split = LT[num].split(':');
   AT_split = AT[num].split(':');
+
+  // 単一行の時の処理
+  if(LT_split == 0){
+    LT_split = LT.split(':');
+  }
+  if(AT_split == 0){
+    AT_split = AT.split(':');
+  }
+  console.log(LT_split);
+  console.log(AT_split);
   // 行ごとの時間と分を各変数に格納
     Ltime[num] = new Date('','','',LT_split[0],LT_split[1]);
+
     Atime[num] = new Date('','','',AT_split[0],AT_split[1]);
-    totaltime[num] = Ltime[num] - Atime[num];
+
+    if(Ltime[num] < Atime[num]){
+      Ltime[num] = new Date('','','1',LT_split[0],LT_split[1]);
+    }
+
+    // ここからが時間ごとの振り分け
+    // 9:00に出勤
+    if(asa <= Atime[num] && Atime[num] <= teji){
+      // 17:45までに退勤
+      if(asa <= Ltime[num] && Ltime[num] <= teji){
+        console.log('9:00-17:45')
+        work_time =  Ltime[num] - Atime[num];
+        // 時刻に加工
+        if(work_time >= 1000*60){
+          for(work_hour = 0 ; work_time >= 1000*60*60 ; work_hour++){
+            work_time = work_time - 1000*60*60;
+          }
+          for(work_min = 0 ; work_time >= 1000*60 ; work_min++){
+            work_time = work_time - 1000*60;
+          }
+          work_time = work_hour + ':' + work_min
+        }
+        if(long_time >= 1000*60){
+          for(work_hour = 0 ; long_time >= 1000*60*60 ; work_hour++){
+            long_time = long_time - 1000*60*60;
+          }
+          for(work_min = 0 ; long_time >= 1000*60 ; work_min++){
+            long_time = long_time - 1000*60;
+          }
+          long_time = work_hour + ':' + work_min
+        }
+        if(night_time >= 1000*60){
+          for(work_hour = 0 ; night_time >= 1000*60*60 ; work_hour++){
+            night_time = night_time - 1000*60*60;
+          }
+          for(work_min = 0 ; night_time >= 1000*60 ; work_min++){
+            night_time = night_time - 1000*60;
+          }
+          night_time = work_hour + ':' + work_min
+        }
+      // 22:00までに退勤
+      }else if(teji < Ltime[num] && Ltime[num] < juji){
+        console.log('9:00-22:00')  
+      } 
+      // 0:00までに退勤
+      else if(juji <= Ltime[num] && Ltime[num] < juniji2){
+        console.log('9:00-0:00')  
+      } 
+      // 5:00までに退勤
+      else if(juniji <= Ltime[num] && Ltime[num] < goji){
+        console.log('9:00-5:00')  
+      } 
+      // 翌9:00までに退勤
+      else if(goji2 <= Ltime[num] && Ltime[num] < asa2){
+        console.log('9:00-9:00')  
+        work_time =  teji - Atime[num];
+        long_time = (juji - teji) + (Ltime[num] - goji2);
+        night_time = (juniji2 - juji) + (goji2 - juniji2); 
+        // 時刻に加工
+        if(work_time >= 1000*60){
+          for(work_hour = 0 ; work_time >= 1000*60*60 ; work_hour++){
+            work_time = work_time - 1000*60*60;
+          }
+          for(work_min = 0 ; work_time >= 1000*60 ; work_min++){
+            work_time = work_time - 1000*60;
+          }
+          work_time = work_hour + ':' + work_min
+        }
+        if(long_time >= 1000*60){
+          for(work_hour = 0 ; long_time >= 1000*60*60 ; work_hour++){
+            long_time = long_time - 1000*60*60;
+          }
+          for(work_min = 0 ; long_time >= 1000*60 ; work_min++){
+            long_time = long_time - 1000*60;
+          }
+          long_time = work_hour + ':' + work_min
+        }
+        if(night_time >= 1000*60){
+          for(work_hour = 0 ; night_time >= 1000*60*60 ; work_hour++){
+            night_time = night_time - 1000*60*60;
+          }
+          for(work_min = 0 ; night_time >= 1000*60 ; work_min++){
+            night_time = night_time - 1000*60;
+          }
+          night_time = work_hour + ':' + work_min
+        }
+        console.log('時間処理の終了')
+      }
+
+    console.log('work_time: ' + work_time);
+    console.log('long_time: ' + long_time);
+    console.log('night_time: ' + night_time);
+  }
+  
+    // ここまでが時間ごとの振り分け
+
+
     
+
+  totaltime[num] = Ltime[num] - Atime[num];
+  
     timesum = timesum + totaltime[num]
 
     // 勤務時間の計算
@@ -151,6 +286,7 @@ if(O < I){
   timesum = hikaku_hour + ':' + hikaku_min
   console.log('timesum: ' + timesum);
 
+
   // ちゃんと書けているかの判断
   if(total == timesum){
     console.log("正しく記入されている")
@@ -158,32 +294,44 @@ if(O < I){
     console.log("記入内容に間違いが存在する")
   }
 
-  // 単一行と複数行の登録
-  if(!Array.isArray(p)){
-    p=1
-  }
-  console.log(p)
-  for(i ; i < p.length ; i++){
+  // (await function(){
+  //   query = {
+  //     text:'INSERT INTO date_dt( year, month, day, id, att_time, leave_time, rest_time, work_time ) VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
+  //     values: [ date_sample[0],
+  //               date_sample[1],
+  //               date_sample[2],
+  //               req.session.login,
+  //               req.body.AttTime,
+  //               req.body.LeaveTime,
+  //               req.body.RestTime,
+  //               total
+  //             ]
+  //   }
+  // })();
 
-    query = {
-      text: 'INSERT INTO time_dt(id, date, att_time, leave_time, rest_time, product_code, work_content, work_time) VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
-      values: [ req.session.login,
-                req.body.date, 
-                req.body.AttTime, 
-                req.body.LeaveTime, 
-                req.body.RestTime, 
-                req.body.project_code[i], 
-                req.body.work_content[i], 
-                // date,
-                total
-              ],
-    }
-    await pool.query(query,  function (err, res) {
-      if(err){
-        console.log(err.stack)
-      }   
-    })
-  }
+  // // 単一行と複数行の登録
+  // if(!Array.isArray(p)){
+  //   p=1
+  // }
+  // console.log(p)
+  // for(i ; i < p.length ; i++){
+
+  //   query = {
+  //     text: 'INSERT INTO time_dt( year, month, day, project_code, work_content, normal_work_time, long_work_time, night_work_time) VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
+  //     values: [ date_sample[0],
+  //               date_sample[1],
+  //               date_sample[2],
+  //               req.body.project_code[i], 
+  //               req.body.work_content[i],
+  //               total
+  //             ],
+  //   }
+  //   await pool.query(query,  function (err, res) {
+  //     if(err){
+  //       console.log(err.stack)
+  //     }   
+  //   })
+  // }
 
   let test = {
     text: 'select * from time_dt',
